6abd3445af16da29fa5093fb49fd1c7050197dc5
Josh Santos <josh@omnidapps.com>
2022-06-06 02:45:35 -0400

    Update odoo values

--- odoo/install.sh
+++ odoo/install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install erp bitnami/odoo -f values.yaml -f values-k0s.yaml
-
+helm install erp bitnami/odoo -f values/common.yaml
